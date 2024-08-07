3d4fa8f9d1a4a83fe16b271f07aa4f924948a260
Kirill Mikhailov <kicode@mail.ru>
2024-08-07 10:17:17 +0300

    fix

--- docker/app.sh
+++ docker/app.sh
@@ -2,4 +2,4 @@
 
 alembic upgrade head
 
-guinicorn main:app --workers 4 --workers-class uvicorn.workers.UvicornWorkers --bind=0.0.0.0:8000
+gunicorn main:app --workers 4 --workers-class uvicorn.workers.UvicornWorkers --bind=0.0.0.0:8000
